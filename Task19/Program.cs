ed6c5e0ea791c9dd3e11519e19ba8b6c7b18bd9a
Linar <alr86@mail.ru>
2022-07-05 12:50:46 +0300

    Added Task 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Palindrome(string number)
+{
+  if (number[0]==number[4] && number[1]==number[3])
+  {
+    Console.WriteLine("Yes, your number is palindrome.");
+  }
+  else Console.WriteLine("No, your number isn't palindrome.");
+}
+Console.Write("Input five-digit number: ");
+string number = Convert.ToString(Console.ReadLine());
+if (number.Length == 5)
+{
+  Palindrome(number);
+}
+else Console.WriteLine("Input the correct number");
