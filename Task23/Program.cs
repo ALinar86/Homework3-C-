683460312c57e3f66da828acda5ecddd4dc66398
Linar <alr86@mail.ru>
2022-07-05 13:13:25 +0300

    Added Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿void kub(int N)
+{
+    for (int i = 1; i <= N; i++ )
+    {
+            Console.Write(i * i * i  + " ");
+    }
+}
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+kub(x);
